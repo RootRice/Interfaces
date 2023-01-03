c23211123b7d832f5fc30757855c3b547e6465a1
RootRice <michaelhindle95@gmail.com>
2023-01-03 14:09:52 +0000

    Added test code for drawing elements

--- Interfaces.cpp
+++ Interfaces.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Panel.h"
+#include "Shape_Element.h"
+#include "GUI.h"
 
 int main()
 {
     sf::RenderWindow my_window(sf::VideoMode(600, 600), "Interfaces!");
-    //NEXT UP: CHECK SHAPE_ELEMENT ACTUALLY WORKS
+    GUI my_gui(my_window);
+
+    Panel::Panel_Properties panel_props{ sf::Vector2f(200, 300), Panel::anchor_point::top_right };
+    Panel& panel_to_edit = my_gui.Add_Panel(panel_props);
+    Panel_Element::Element_Properties element_props{ sf::Vector2f(50, 50), sf::Vector2f(25, 25), sf::Vector2f(100,0) };
+    Shape_Element element(sf::Color::Red, element_props);
+    panel_to_edit.Add_Element(element);
+
+    while (my_window.isOpen())
+    {
+        my_window.clear();
+        my_gui.Draw_Panels();
+        my_window.display();
+    }
+    
 }
